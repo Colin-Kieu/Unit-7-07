587ee13ed5cf1c0cefa16511170a43bad5a26527
ColinKieu <colin.kieu@ocsbstudent.ca>
2021-03-26 14:38:21 +0000

    linter

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('button').addEventListener('click', multiply)
-//button connects to the function
+// button connects to the function
 let num1 = 0
 let num2 = 0
 let counter = 0
@@ -10,11 +10,11 @@ function multiply () {
   num2 = document.getElementById('input2').value
   num1 = parseInt(num1)
   num2 = parseInt(num2)
-// take our inputs and converts them into integers
+  // take our inputs and converts them into integers
   for (counter = 0; counter < num1; counter++) {
     answer = answer + num2
   // this is the for loop
-  } 
+  }
   document.getElementById('answer').innerHTML = answer
 // this will display the answer in a pragrapgh tag
 }
